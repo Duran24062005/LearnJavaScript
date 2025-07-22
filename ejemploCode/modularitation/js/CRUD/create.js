b8008e8279cb184b9fda3d3751525f07368a0c8f
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-07-22 17:40:33 -0500

    feat: :fire: updated

--- ejemploCode/modularitation/js/CRUD/create.js
+++ ejemploCode/modularitation/js/CRUD/create.js
@@ -1,5 +1,38 @@
 import { products } from "../db/products.js"
 
 export default function createProduct() {
-    alert('Hola mundo')
+    let data = parseInt(prompt(`
+        ===============================
+        === Bienvenido a VerdeFruta ===
+        ===============================
+        Elige una opción para continuar:
+            1. Ver stock completo.
+            2. Ver todo el stock.
+            3. Salir
+        ===============================
+        `));
+    switch (data) {
+        case 1:
+            alert(JSON.stringify(products, null, 2));
+            break;
+        
+        case 2:
+            for (let index = 0; index < products.length; index++) {
+                let tatalDate = `
+                name: ${products[index].name},
+                description: ${products[i].description},
+                price: ${products[i].price}
+                `;
+            };
+            alert(JSON.stringify(tatalDate, null, 2));
+            break;
+        
+        case 3:
+            alert(`Gracias por ver.`);
+            break;
+    
+        default:
+            alert(`Por favor ingresa una opción valida.`);
+            break;
+    }
 };
