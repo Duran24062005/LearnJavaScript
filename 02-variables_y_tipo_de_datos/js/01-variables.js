9dd5f7c7fd01975fcb76f7fff79f2b2cca080388
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-07-26 21:10:33 -0500

    feat: :herb: added to code

---
+++ 02-variables_y_tipo_de_datos/js/01-variables.js
@@ -0,0 +1,15 @@
+// Variables
+
+// Var: crea variables globales siempre:
+// No solo funciona en bloques, funcioan en cualquier parate del codigo
+var variable = "Hola mundo";
+
+// Let: crea variables locales siempre:
+//
+let apellido = variable;
+
+// const: crea constantes:
+// tambien tienen un scope local
+const lastVar = 10;
+
+console.log(apellido + ' ' + lastVar);
