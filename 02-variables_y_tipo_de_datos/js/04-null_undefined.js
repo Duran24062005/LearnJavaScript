733ad56ac32d41035d97441c683bf9803141abbf
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-07-27 15:39:41 -0500

    feat: :sparkles: logiud

---
+++ 02-variables_y_tipo_de_datos/js/04-null_undefined.js
@@ -0,0 +1,13 @@
+// null & undefined
+/* 
+undefined: Variables no definidas.
+null:   Cuando declaramos especificamente la variable como null
+*/
+
+// undefined
+let variable;
+console.log(variable);
+
+// null
+let nulo = null;
+console.log(nulo);
---
+++ 02-variables_y_tipo_de_datos/js/05-strings.js
@@ -0,0 +1,13 @@
+// Cadenas de texto o Strings
+let nombre = "Alexi Dg";
+
+
+// Funcionalidades para trabajar con cadenas de texto
+// Los metodos son marticularidades, un nombre técnico que se le dan a las 
+//  funcionalidades que se pueden ejecutar sobre un texto
+let nombreCompleto = "Alexi Duran Gomez";
+
+// Medir la longitud de una cadena de texto: 
+// length = logitud: con este metodo podemos saber la longitud 
+// en caracteres del texto
+console.log(nombreCompleto.length);
