fe32d17495363c22d23fd424bcf234454f08088c
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-07-27 20:38:43 -0500

    feat: :herb: added aritmetics operators

---
+++ 03-operadores/01-operadores-aritmeticos.js
@@ -0,0 +1,50 @@
+// Operadores Aritmeticos
+let numeroA = 14;
+let numeroB = 32;
+
+// Suma +
+let resultadoSuma = numeroA + numeroB;
+console.log("El valor de la suma es: " + resultadoSuma);
+
+// Resta -
+let resultadoResta = numeroB - numeroA;
+console.log("El valor de la resta es: " + resultadoResta);
+
+// Multiplicación *
+let resultadoMultiplicacion = numeroA * numeroB;
+console.log("El valor de la multiplicación es: " + resultadoMultiplicacion);
+
+// División /
+let resultadoDivision = numeroB / numeroA;
+console.log("El valor de la división es: " + resultadoDivision);
+
+// Modulo %: Entrega el restante de una división
+let Modulo = numeroB % numeroA;
+console.log(Modulo);
+
+// Exponenciación **
+let numeroC = 2;
+let numeroD = 3;
+let resultadoExponenciacion = numeroC ** numeroD;
+console.log("El valor de la exponenciación es: " + resultadoExponenciacion);
+
+// Operación convinada
+let operacionConbinada = 5 + 10 * 2;
+console.log(operacionConbinada);
+
+// Agrupación - Precedencias
+// Los parentesis () tienen presedencias
+let operacionConbinada2 = (5 + 10) * 2;
+console.log(operacionConbinada2);
+
+let operacionConbinada3 = 5 + (10 * 2);
+console.log(operacionConbinada3);
+
+
+
+
+
+
+
+// Operadores de comparación
+// Operadores lógicos
