dfaf375fce3ff1dfde8b71b705bddaa357ede496
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-07-28 07:41:05 -0500

    feat: :herb: added comparation operators

--- 03-operadores/02-operadores-comparacion.js
+++ 03-operadores/02-operadores-comparacion.js
@@ -1,1 +1,42 @@
 // Operadores de comparación
+
+//  De igualdad simple ==
+console.log(5 == 5);
+console.log(5 == "5");
+console.log(5 == 15);
+console.log("hola" == "Hola");
+console.log("hola" == "hola");
+
+//  De igualdad estricta ===, compara tambien el tipo de dato
+console.log('\n');
+console.log(12 === 12);
+console.log(5 === "5");
+
+// Mayor que >
+console.log(4 > 2);
+console.log(4 > 22);
+console.log(4 > 4);
+
+// Mayor o igual que >=
+console.log('\n');
+console.log(4 >= 4);
+
+// Menor que <
+console.log('\n');
+console.log(4 < 2);
+console.log(4 < 22);
+console.log(4 < 4);
+
+// Menor o igual que <=
+console.log(4 <= 4);
+
+// Distinto qué !=, simple
+console.log('\n');
+console.log('hola' != 'Hola');
+
+// Distinto qué !==, simple
+console.log('\n');
+console.log('hola' !== 'Hola');
+console.log(5 !== '5');
+console.log(5 !== 5);
+console.log("5" !== "5");
