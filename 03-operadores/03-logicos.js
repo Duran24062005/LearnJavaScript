9c45b8ae57a67e0fd24293a994c66136f9938e24
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-08-08 17:44:22 -0500

    feat: :fire: added logic operators

--- 03-operadores/03-logicos.js
+++ 03-operadores/03-logicos.js
@@ -1,1 +1,20 @@
 // Operadores Lógicos
+/* 
+    clima => soleado
+    dia => viernes o si es sabado
+    horario => antes de las 6
+ */
+
+let clima = "soleado";
+let dia = "viernes";
+let horario = 2;
+
+// AND - &&
+console.log(clima === "soleado" && dia === "viernes" && horario < 6);
+
+// OR - ||
+console.log(dia === "viernes" || dia === "sabado");
+
+// NOT - !
+let operator = true;
+console.log(!operator);
