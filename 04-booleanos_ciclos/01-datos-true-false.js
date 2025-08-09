a0073139bf8f674fcdb154750f43e3a98c0011d2
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-08-08 20:41:51 -0500

    feat: :fire: added true and false booleans

---
+++ 04-booleanos_ciclos/01-datos-true-false.js
@@ -0,0 +1,32 @@
+// Booleanos - TRUE and FALSE
+
+let positivo = true;
+let negativo = false;
+
+
+// Truthy
+/* 
+    - Todos los strings
+    - Cualquier numero mayor a 0
+ */
+
+let str = "f";
+let str2 = 'f';
+let str3 = " ";
+let str4 = ' ';
+let num = n>0;
+
+// Falsy
+/* 
+    - Todos los strings basios, ya sea con comila doble o simple
+        no cuentan los espacios bacios.
+    - El numero 0
+    - Null
+ */
+
+let strings = "";
+let string2 = '';
+let string3 = "";
+let string4 = '';
+let numb = 0;
+let mulo = null;
