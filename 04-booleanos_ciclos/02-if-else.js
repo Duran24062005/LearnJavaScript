34cd976237f8fddce90256dd1600650cbcd7c3d9
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-08-10 10:39:18 -0500

    feat: :sparkles: added if else

---
+++ 04-booleanos_ciclos/02-if-else.js
@@ -0,0 +1,18 @@
+// IF and ELSE
+let clima = "soleado";
+let clima2 = "lluvioso";
+let horario = 3;
+
+if (clima === "soleado" && horario < 6) {
+    console.log('Puedes salir');
+};
+
+if (clima2 === "soleado" && horario < 6) {
+    let message = 'demos un paseo';
+    console.log(`EL clima esta ${clima}, entonces ${message}`);
+} else {
+    console.log('No puedes salir');
+};
+
+// TODO
+// falta el 3, 4, y 5
