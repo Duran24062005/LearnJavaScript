b8f9831633c4b1e5f0101d50a8459d22d70102d4
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-08-15 21:38:05 -0500

    feat: :fire: added introduction to arrays

---
+++ 07-arrays/01-introduction.js
@@ -0,0 +1,16 @@
+// ARRAYS
+
+/*
+    Arrays are a very special type of data.
+*/
+
+let myPets = ['koen', 'Muñeca', 'azabache'];
+console.log(myPets);
+
+// myArray.push('Que hubo pues');
+// console.log(myArray[0]);
+// myArray.splice(3, 4);
+
+for(let i = 0; i < myPets.length; i++) {
+    console.log(`My pet ${myPets[i]}`);
+}
