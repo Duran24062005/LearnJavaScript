1cc09384385efabbe95cb528d10b9da70cb22ff3
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-08-18 22:02:21 -0500

    feat: :fire: added li ob keys and values

--- 08-objetos_literales/02-key-value.js
+++ 08-objetos_literales/02-key-value.js
@@ -1,1 +1,39 @@
-// Key Values
+// Key Values+
+
+const marieSmithers = {
+    nombre: "Marie",
+    apellido: "Smithers",
+    edad: 27,
+    email: "marie@smithers.com",
+    "ubicacion": {
+        ciudad: "ciudad de Mexico",
+        pais: "Mexico"
+    },
+    "ciudad_nacimeinto": "mexico city",
+    "ciudad nacimeinto": "Ciudad de mexico",
+    34: 50
+};
+
+console.log(typeof marieSmithers);
+console.log(marieSmithers);
+
+console.log(typeof marieSmithers.ciudad_nacimeinto);
+console.log(marieSmithers.ciudad_nacimeinto);
+
+console.log(typeof marieSmithers["ciudad nacimeinto"]);
+console.log(marieSmithers["ciudad nacimeinto"]);
+
+console.log(typeof marieSmithers[34]);
+console.log(marieSmithers[34]);
+
+const johnDoe = {
+    name: "John Doe",
+    age: 40,
+    email: "john@doe.com",
+    "ubication": {
+        ciudad: "Bogota",
+        pais: "Colombia"
+    },
+    "pets": [ "lazzy", "Priet" ],
+    isMarried: null
+};
