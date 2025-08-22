a448778f4e7c9fbb4dba21045e76361a30eba43c
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-08-21 21:40:09 -0500

    feat: :fire: added class and instances

---
+++ 09-POO/02-class-instances.js
@@ -0,0 +1,28 @@
+// CLASS and INSTANCES
+
+// Class
+class People {
+    // Constructor - Builder
+    // Atributos
+    // Metodos
+}
+
+// Instance = Creation of an object from a class (template)
+const carl = new People(); // This is an Object
+const max = new People();
+const people = {};
+
+console.log(carl);
+// People {}
+console.log(max);
+// People {}
+console.log(people);
+// {}
+
+class Mascota {
+
+}
+
+const chays = new Mascota();
+console.log(chays);
+// Mascota {}
