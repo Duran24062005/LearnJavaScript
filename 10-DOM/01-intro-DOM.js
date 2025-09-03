f8154464b77d8e3301651a6bc16cedf3904305a9
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-09-03 18:17:40 -0500

    feat: :sparkles: added intro DOM

---
+++ 10-DOM/01-intro-DOM.js
@@ -0,0 +1,8 @@
+// Introduction to the DOM (Document Object Model)
+
+// The DOM is a programming interface for web documents. It represents the page so that programs can change the 
+// document structure, style, and content. The DOM represents the document as a tree of nodes.
+// Example: Accessing and Modifying DOM Elements
+
+
+
